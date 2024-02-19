1462551f4243ecd221c530ff340825d1f94efc9b
Francisco Velez <42354796+fram446742@users.noreply.github.com>
2024-02-19 13:38:18 +0100

    Update install_node_local.sh

--- install_node_local.sh
+++ install_node_local.sh
@@ -5,6 +5,43 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
+# Function to prompt the user for cleaning the folder
+prompt_clean_folder() {
+    read -p "The $1 folder already exists. Do you want to clean it before continuing? (yes/no): " choice
+    case "$choice" in
+    yes | YES | y | Y)
+        # Clean the folder
+        echo "Cleaning $1 folder..."
+        rm -rf $2/*
+        ;;
+    no | NO | n | N)
+        echo "Skipping cleaning of $1 folder. Proceeding..."
+        ;;
+    *)
+        echo "Invalid choice. Please enter yes or no."
+        prompt_clean_folder "$1" "$2"
+        ;;
+    esac
+}
+
+# Function to prompt the user for hiding a folder
+prompt_hide_folder() {
+    read -p "Do you want to hide the $1 folder? (yes/no): " choice
+    case "$choice" in
+    yes | YES | y | Y)
+        # Rename the folder to make it hidden
+        mv $1 $2
+        ;;
+    no | NO | n | N)
+        echo "Keeping $1 folder visible."
+        ;;
+    *)
+        echo "Invalid choice. Please enter yes or no."
+        prompt_hide_folder "$1" "$2"
+        ;;
+    esac
+}
+
 # Check if curl and grep are installed
 if ! command_exists curl || ! command_exists grep; then
     echo "curl or grep is not installed. Installing..."
@@ -22,28 +59,9 @@ fi
 # Continue with the rest of your script here
 echo "Both curl and grep are installed. Proceeding with the script..."
 
-# Function to prompt the user for cleaning the folder
-prompt_clean_folder() {
-    read -p "The ~/nodejs folder already exists. Do you want to clean it before continuing? (yes/no): " choice
-    case "$choice" in
-    yes | YES | y | Y)
-        # Clean the folder
-        echo "Cleaning ~/nodejs folder..."
-        rm -rf ~/nodejs/*
-        ;;
-    no | NO | n | N)
-        echo "Skipping cleaning of ~/nodejs folder. Proceeding..."
-        ;;
-    *)
-        echo "Invalid choice. Please enter yes or no."
-        prompt_clean_folder
-        ;;
-    esac
-}
-
 # Check if ~/nodejs folder exists
 if [ -d ~/nodejs ]; then
-    prompt_clean_folder
+    prompt_clean_folder "~/nodejs" "~/nodejs"
 fi
 
 # Create nodejs directory in the home directory
@@ -74,14 +92,21 @@ source ~/.bashrc
 echo "NODE_HOME: $NODE_HOME"
 echo "PATH: $PATH"
 
+# Prompt for hiding the ~/nodejs folder
+prompt_hide_folder "~/nodejs" "~/.nodejs"
+
 # Check if ~/.n folder exists
 if [ -d ~/.n ]; then
-    prompt_clean_folder
+    prompt_clean_folder "~/.n" "~/.n"
 fi
 
-# Install and use 'n' to manage Node.js versions
-echo "Installing 'n' to manage Node.js versions..."
+# Create .n directory in the home directory
+echo "Creating ~/.n directory..."
 mkdir -p ~/.n
+
+# Prompt for hiding the ~/.n folder
+prompt_hide_folder "~/n" "~/.n"
+
 export N_PREFIX=~/.n
 export PATH="$N_PREFIX/bin:$PATH"
 
